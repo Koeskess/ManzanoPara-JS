c42b70ed6f5e88fcb50b022ffd2f3e9274295590
Leticia Koeskes <105087823+Koeskess@users.noreply.github.com>
2022-12-16 03:17:56 -0300

    Add files via upload

---
+++ JS - PARA/PARA B/script.js
@@ -0,0 +1,4 @@
+ var numero = parseInt(prompt("Digite um número: "));
+    for (var contadora = 1; contadora <= 10; contadora++) {
+        document.write(numero + " X " + contadora + " = " + contadora * numero + "</br>");
+    }
